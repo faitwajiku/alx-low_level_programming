f52595a3f502e786072adb63fa3e1585490e50d8
faitwajiku <faitwajiku@gmail.com>
2023-05-25 17:41:01 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make the alphabet
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+
+char b;
+
+for (b = 'a'; b <= 'z'; b++)
+{
+
+_putchar(b);
+
+}
+
+_putchar('\n');
+}
