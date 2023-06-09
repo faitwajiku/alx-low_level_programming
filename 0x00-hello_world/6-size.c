b16d875f93c54f4337d2b7bad61636d8bd0f48d8
faitwajiku <faitwajiku@gmail.com>
2023-06-09 11:56:33 -0700

    hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,5 @@ printf("Size of an int: %zd byte(s)\n", sizeof(int));
 printf("Size of a long int: %zd byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %zd byte(s)\n", sizeof(long long int));
 printf("Size of a float: %zd byte(s)\n", sizeof(float));
-return 0;
+return (0);
 }
