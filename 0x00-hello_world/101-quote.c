e749735a99cadb50120f816ff7357aaaea3c859e
faitwajiku <faitwajiku@gmail.com>
2023-06-15 00:46:07 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,11 +10,8 @@
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t len = sizeof(message) - 1;
-
-	write(STDERR_FILENO, message, len);
-
-	return (1);
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ssize_t len = sizeof(message) - 1;
+write(STDERR_FILENO, message, len);
+return (1);
 }
-
