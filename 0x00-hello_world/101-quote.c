a79a47d35d6a2e5c3050446b6d572c22985e106d
faitwajiku <faitwajiku@gmail.com>
2023-06-15 00:43:26 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,20 @@
 #include <unistd.h>
 
-int main() {
-    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, sizeof(message) - 1);
-    return 1;
+/**
+ * main - Entry point
+ *
+ * Description: Prints the message "and that piece of art is useful"
+ *              - Dora Korpar, 2015-10-19 to the standard error.
+ *
+ * Return: 1 (Exit status)
+ */
+int main(void)
+{
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t len = sizeof(message) - 1;
+
+	write(STDERR_FILENO, message, len);
+
+	return (1);
 }
 
